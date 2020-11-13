857d3b4c0b218a9cc49dde9bcefde42fec84ae29
Vinay Koshy <vinyoshy2@gmail.com>
2020-11-13 16:33:18 -0600

    Can add likes on projectdetails

--- src/pages/project_details/UserPhotos.js
+++ src/pages/project_details/UserPhotos.js
@@ -3,28 +3,53 @@ import "./UserPhotos.css";
 import photosJSON from "../../data/user_photos.json";
 import {getEntryFromID} from "../../utils/utils.js";
 
-function UserPhotos(props) {
+export default class UserPhotos extends React.Component {
 
-    var entries = props.photo_ids.map(id => getEntryFromID(id, photosJSON));
-    return (
-	<div id="UserPhotos">
-	    {
-	        entries.map(entry => 
-                    <div class="entry">
-			<div class="photo" style={{backgroundImage: "url(" + "/" + entry["plant_pic"] + ")"}}/>
-  		        <div class="user-info">
-			    <div class="profile-pic" style={{backgroundImage: "url(" + "/" + entry["profile_pic"] + ")"}}/>
-			    <div class="username"> Uploaded by {entry["user"]}</div>
+    constructor(props) {
+        super(props)
+        this.entries = props.photo_ids.map(id => getEntryFromID(id, photosJSON));
+	var likes = this.entries.map(entry => entry["likes"]);
+	var likes_updated = likes.map(like => false);
+	this.state = {
+	    likes: likes,
+	    updated: likes_updated
+	}
+    }	
+
+    clickHandler(id) {
+	if (!this.state.updated[id]) {
+	    var new_likes = [...this.state.likes];
+            new_likes[id] = new_likes[id] + 1;
+            this.setState(
+                { likes: new_likes  }
+	    );
+	    var new_updated = [...this.state.updated];
+            new_updated[id] = true;
+	    this.setState(
+	        {updated: new_updated}
+	    );
+	}
+    }
+    render() {
+        return (
+	    <div id="UserPhotos">
+	        {
+	            this.entries.map(entry => 
+                        <div class="entry">
+			    <div class="photo" style={{backgroundImage: "url(" + "/" + entry["plant_pic"] + ")"}}/>
+  		            <div class="user-info">
+			        <div class="profile-pic" style={{backgroundImage: "url(" + "/" + entry["profile_pic"] + ")"}}/>
+			        <div class="username"> Uploaded by {entry["user"]}</div>
+		            </div>
+		            <div class="like">
+	                        <input type="image" class="like-button" onClick={() => this.clickHandler(entry["id"])} src={!this.state.updated[entry["id"]] ? "/thumb.png" : "/thumb_green.png"}/>
+	                        <div class="like_count">{this.state.likes[entry["id"]]}</div>
+			    </div>
 		        </div>
-		        <div class="like">
-	                    <input type="image" class="like-button" src={"/thumb.png"}/>
-	                    <div class="like_count">{entry["likes"]}</div>
-			</div>
-		    </div>
-	    )
-	    }
-	</div>
-    )
+	            )
+	        }
+	    </div>
+        )
+    }
 }
 
-export default UserPhotos;
