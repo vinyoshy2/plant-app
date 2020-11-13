73960dc1bd5a60a3e586c6b4044a331f53e69854
Jundong Chen <jundong2@illinois.edu.com>
2020-11-13 00:47:34 -0600

    add untracked folder

---
+++ src/pages/plant_of_the_day/HeroElementsIndex.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import { Button } from '../plant_of_the_day/ButtonElements.js';
+
+import Video from '../plant_of_the_day/Videos/video.mp4';
+import {
+  HeroContainer,
+  HeroBg,
+  VideoBg,
+  HeroContent,
+  HeroH1,
+  HeroP,
+  HeroBtnWrapper,
+  ArrowForward,
+  ArrowRight
+} from './HeroElements';
+
+function HeroSection() {
+  const [hover, setHover] = useState(false);
+
+  const onHover = () => {
+    setHover(!hover);
+  };
+  return (
+    <HeroContainer id='home'>
+      <HeroBg>
+        <VideoBg playsInline autoPlay loop muted src={Video} type='video/mp4' />
+      </HeroBg>
+      <HeroContent>
+        <HeroH1>Plant of the day</HeroH1>
+        <HeroP>
+          Scorll down to see the best plant for the day!
+        </HeroP>
+        <HeroBtnWrapper>
+          <Button
+            to='about'
+            smooth={true}
+            duration={500}
+            spy={true}
+            exact='true'
+            offset={-80}
+            primary='true'
+            dark='true'
+            onMouseEnter={onHover}
+            onMouseLeave={onHover}
+          >
+            Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+          </Button>
+        </HeroBtnWrapper>
+      </HeroContent>
+    </HeroContainer>
+  );
+}
+
+export default HeroSection;
---
+++ src/pages/plant_of_the_day/PlantOfTheDay.js
@@ -0,0 +1,12 @@
+import React, { useState } from 'react'
+import HeroSection from '../plant_of_the_day/HeroElementsIndex';
+
+function PlantOfTheDay() {
+  return (
+    <>
+      <HeroSection/>
+    </>
+  );
+}
+
+export default PlantOfTheDay;
