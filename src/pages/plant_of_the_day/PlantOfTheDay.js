fd4a89aef15cbd2b7e52d99910e75baae03f3b2a
Jundong Chen <jundong2@illinois.edu.com>
2020-11-18 00:53:44 -0600

    header

--- src/pages/plant_of_the_day/PlantOfTheDay.js
+++ src/pages/plant_of_the_day/PlantOfTheDay.js
@@ -1,6 +1,7 @@
 import React, { useState } from 'react'
 import HeroSection from '../plant_of_the_day/HeroElementsIndex.js';
 import InfoSection from '../plant_of_the_day/InfoElementsIndex.js';
+import NavBarElementIndex from '../../utils/NavBar/NavBarElementsIndex'
 import {
   homeObjOne,
   homeObjTwo,
@@ -11,6 +12,7 @@ import {
 function PlantOfTheDay() {
   return (
     <>
+      <NavBarElementIndex/>
       <HeroSection/>
       <InfoSection {...homeObjOne} />
       <InfoSection {...homeObjTwo} />
