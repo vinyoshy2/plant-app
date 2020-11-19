4ba4ba28a2f90d201d8e7b44b118202e2645a2df
RuiRitaYang <ry2@illinois.edu>
2020-11-19 14:54:02 -0600

    [CareReminder][Fix] Add missed file.

---
+++ src/pages/profile/CareReminderDownload.js
@@ -0,0 +1,13 @@
+import React, {Component} from "react";
+import "./Profile.css"
+
+class CareReminderDownload extends Component {
+  render() {
+    return (
+      <a href='/example_cal.ics' download className='download_button'>Download Calendar</a>
+    );
+  }
+
+}
+
+export default  CareReminderDownload;
