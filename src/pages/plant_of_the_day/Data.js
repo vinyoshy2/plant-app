eebf96fb0fc2303713f346cebc2ff475ceedf05e
Jundong Chen <jundong2@illinois.edu.com>
2020-11-18 18:14:43 -0600

    add infoSection in dailyplant page

---
+++ src/pages/plant_of_the_day/Data.js
@@ -0,0 +1,91 @@
+export const homeObjOne = {
+    id: 'Phalaenopsis',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Nov.19: ',
+    headline: 'Phalaenopsis',
+    description:
+      'Attributes go here',
+    buttonLabel: 'Match projects with the Phalaenopsis',
+    imgStart: true,
+    img: require('../plant_of_the_day/images/Spider-Plant.jpeg'),
+    alt: 'Phalaenopsis',
+    dark: false,
+    primary: false,
+    darkText: true
+  };
+
+export const homeObjTwo = {
+    id: 'Spider Plant',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Nov.18: ',
+    headline: 'Spider Plant',
+    description:
+      'Attributes go here',
+    buttonLabel: 'Match projects with the Spider Plant',
+    imgStart: false,
+    img: require('../plant_of_the_day/images/Spider-Plant.jpeg'),
+    alt: 'Spider Plant',
+    dark: true,
+    primary: true,
+    darkText: false
+  };
+  
+  export const homeObjThree = {
+    id: 'Aloe Vera',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Nov.17',
+    headline: 'Aloe Vera',
+    description:
+      'Attributes go here',
+    buttonLabel: 'Match projects with the Aloe Vera',
+    imgStart: true,
+    img: require('../plant_of_the_day/images/Aleo-Vera.jpeg'),
+    alt: 'Aloe Vera',
+    dark: false,
+    primary: false,
+    darkText: true
+  };
+  
+  export const homeObjFour = {
+    id: 'Mojito Mint',
+    lightBg: false,
+    lightText: true,
+    lightTextDesc: true,
+    topLine: 'Nov.16',
+    headline: 'Mojito Mint',
+    description:
+      "Attributes go here",
+    buttonLabel: 'Match projects with the Mojito Mint',
+    imgStart: false,
+    img: require('../plant_of_the_day/images/Mojito-Mint.jpeg'),
+    alt: 'Mojito Mint',
+    dark: true,
+    primary: true,
+    darkText: false
+  };
+
+  export const homeObjFive = {
+    id: 'Lavender Tree',
+    lightBg: true,
+    lightText: false,
+    lightTextDesc: false,
+    topLine: 'Nov.15',
+    headline: 'Lavender Tree',
+    description:
+      'Attributes go here',
+    buttonLabel: 'Match projects with the Lavender Tree',
+    imgStart: true,
+    img: require('../plant_of_the_day/images/Lavender-Tree.jpeg'),
+    alt: 'Lavender Tree',
+    dark: false,
+    primary: false,
+    darkText: true
+  };
+ 
+  
