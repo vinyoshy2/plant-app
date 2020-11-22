10784f47601e73880ff3f60025b2c84fb2239d9e
RuiRitaYang <ry2@illinois.edu>
2020-11-22 11:43:49 -0600

    [Badges] Add hover and grayscale for badges.

--- src/pages/profile/Badges.js
+++ src/pages/profile/Badges.js
@@ -1,11 +1,15 @@
 import React, { Component } from "react";
 import Sidebar from "./SideBar";
 import { Navbar } from '../home/Home.js'
+import BadgeSingle from './BadgeSingle'
+
+import "./Profile.css"
+
 
 class Badges extends Component {
-  // TODO (ruiyang): Add hover for description
-  //     Reference: https://www.npmjs.com/package/react-hover with code
-  //                https://github.com/cht8687/react-hover/blob/master/src/example/Example.js
+  // Hover reference: https://www.npmjs.com/package/react-hover with code
+  //                  https://github.com/cht8687/react-hover/blob/master/src/example/Example.js
+
   render() {
     return (
         <div>
@@ -17,30 +21,56 @@ class Badges extends Component {
               <h2 className='profile-subtitle'>Your First Project</h2>
               <div className='profile-sub-content'>
                 <div className='vertical-frame'>
-                  <img className='badge-image' src={'https://www.flaticon.com/svg/static/icons/svg/610/610333.svg'}/>
-                  <img className='badge-image' src={'https://www.flaticon.com/svg/static/icons/svg/628/628297.svg'}/>
-                  <img className='badge-image' src={'https://www.flaticon.com/svg/static/icons/svg/628/628292.svg'}/>
+                  <BadgeSingle
+                    imglink={'https://www.flaticon.com/svg/static/icons/svg/610/610333.svg'}
+                    description = {"You've added your first project!"}
+                  />
+                  <BadgeSingle
+                    imglink = {'https://www.flaticon.com/svg/static/icons/svg/628/628297.svg'}
+                    description = {"You've started your first project!"}
+                  />
+                  <BadgeSingle
+                    imglink = {'https://www.flaticon.com/svg/static/icons/svg/628/628292.svg'}
+                    description = {"You've taken care of your first project!"}
+                  />
+                  <BadgeSingle
+                    imglink = {'https://www.flaticon.com/svg/static/icons/svg/1042/1042339.svg'}
+                    description = {"You've uploaded your first photo!"}
+                  />
                 </div>
               </div>
 
               <h2 className='profile-subtitle'>Planting Journey</h2>
               <div className='profile-sub-content'>
                 <div className='vertical-frame'>
-                  <img className='badge-image' src={'https://www.flaticon.com/svg/static/icons/svg/628/628283.svg'}/>
-                  <img className='badge-image' src={'https://www.flaticon.com/svg/static/icons/svg/628/628271.svg'}/>
-                  <img className='badge-image' src={'https://www.flaticon.com/svg/static/icons/svg/2830/2830919.svg'}/>
+                  <BadgeSingle
+                    imglink = {'https://www.flaticon.com/svg/static/icons/svg/628/628295.svg'}
+                    description = {"You've finished one project!"}
+                  />
+                  <BadgeSingle
+                    imglink = {'https://www.flaticon.com/svg/static/icons/svg/628/628283.svg'}
+                    description = {"You've finished three projects!"}
+                  />
+                  <BadgeSingle
+                    imglink = {'https://www.flaticon.com/svg/static/icons/svg/2830/2830919.svg'}
+                    description = {"You've finished five projects!"}
+                  />
                 </div>
               </div>
 
-              <h2 className='profile-subtitle'>APPNAME Journey</h2>
+              <h2 className='profile-subtitle'>Thrive Journey</h2>
               <div className='profile-sub-content'>
                 <div className='vertical-frame'>
-                  <img className='badge-image' src={'https://www.flaticon.com/svg/static/icons/svg/628/628295.svg'}/>
-                  <img className='badge-image' src={'https://www.flaticon.com/svg/static/icons/svg/628/628265.svg'}/>
-                  <img className='badge-image' src={'https://www.flaticon.com/svg/static/icons/svg/3629/3629314.svg'}/>
+                  <BadgeSingle
+                    imglink = {'https://www.flaticon.com/svg/static/icons/svg/893/893268.svg'}
+                    description = {"You've (been) connected with other users!"}
+                  />
+                  <BadgeSingle
+                    imglink = {'https://www.flaticon.com/svg/static/icons/svg/1029/1029183.svg'}
+                    description = {"Your photo(s) gets liked by other users!"}
+                  />
                 </div>
               </div>
-
             </div>
           </div>
         </div>
